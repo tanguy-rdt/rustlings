bc3dc1fe07d41450693ff99f07c749b6c49f6578
Tanguy ROUDAUT <tanguy@roudaut.xyz>
2023-01-06 17:04:38 +0100

    quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,18 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(nb_apple: i32) -> i32 {
+    let mut apple_price: i32 = 0;
+
+    if nb_apple <= 40 {
+        apple_price = 2;
+    } else {
+        apple_price = 1;
+    }
+
+    nb_apple * apple_price
+}
 
 // Don't modify this function!
 #[test]
