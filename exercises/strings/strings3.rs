1dbea106750f801e5c1e764572d2b97a09d40125
Tanguy ROUDAUT <tanguy@roudaut.xyz>
2023-01-06 17:03:13 +0100

    strings

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -1,21 +1,24 @@
 // strings3.rs
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let mut my_string = input.to_string();
+    my_string.trim().to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut my_string = input.to_string();
+    my_string.push_str(" world!");
+    my_string
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let mut my_string = input.to_string();
+    my_string.replace("cars", "balloons")
 }
 
 #[cfg(test)]
